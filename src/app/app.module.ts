550dc8660efdaf5f14ff38acfc0fb7f36ecb616a
bolkonksy <bolkonsky@tuta.io>
2016-12-03 16:25:35 +0100

    Implemented RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { CollapseDirective } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { MenuPanelComponent } from './menu-panel/menu-panel.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'login', component: LogInComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,13 +31,19 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     AppComponent,
     QuestionsComponent,
     MenuPanelComponent,
-    NavBarComponent
+    NavBarComponent,
+    UserComponent,
+    HomeComponent,
+    SignUpComponent,
+    LogInComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sign-up/sign-up.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { SignUpComponent } from './sign-up.component';
+
+describe('SignUpComponent', () => {
+  let component: SignUpComponent;
+  let fixture: ComponentFixture<SignUpComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SignUpComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
