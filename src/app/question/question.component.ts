e9b346229d28d554405a37b26a3fb824cf5111ef
gcupko00 <gcupko00@fesb.hr>
2016-12-08 10:39:25 +0100

    Minor update

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
 
-  constructor() { }
+export class QuestionComponent {
+  public title: string = "Question title example";
+  public description: string = "Lorem ipsum dolor sit amet, interdum nulla, nec quam, feugiat nisl. " +
+    "Ac lacinia, a in. Penatibus sit suscipit, vel nam. Et praesent parturient, pharetra nunc mauris. Id arcu.";
+  public date: Date = new Date();
+  public tags: string[] = ["css", "bootstrap", "javascript"];
 
-  ngOnInit() {
+  public rating: number = 14;
+  public answers: number = 3;
+  public views: number = 230;
+
+  constructor() {
   }
-
 }
